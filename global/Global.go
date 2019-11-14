677b2b3fe32c5e10e0e50f3b1a84b4de687908be
yuanxs <yuanxs@95szzl.cn>
2019-11-14 17:44:53 +0800

    201911141744

--- global/Global.go
+++ global/Global.go
@@ -7,9 +7,9 @@ import (
 
 const (
 	//goServiceSupportHelper Version "1.0.7 Build20190823"
-	//PreVersion = "2.0.0 Build20190824"
+	//PreVersion = "2.0.1 Build20191114"
 	//TestVersion = "0.0.0 Build20190101"
-	Version   = "2.0.1 Build20191114"
+	Version   = "0.0.0 Build20190101"
 	Type      = "Z9MdDataTransV2"
 	SecretKey = "Z9MdDataTransV2"
 )
